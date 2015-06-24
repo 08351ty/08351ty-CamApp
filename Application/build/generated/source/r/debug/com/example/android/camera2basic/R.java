5b242944a6bb674d75533c9070d7b84cd93dcaf8
08351ty <pascal.so@gmail.com>
2015-06-24 18:00:03 +0800

    third save
    added left arrow, home UI needs camera preview

--- Application/build/generated/source/r/debug/com/example/android/camera2basic/R.java
+++ Application/build/generated/source/r/debug/com/example/android/camera2basic/R.java
@@ -135,15 +135,17 @@ containing a value of this type.
     }
     public static final class drawable {
         public static final int ic_action_gallery=0x7f020000;
-        public static final int ic_action_takepic=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int tile=0x7f020003;
+        public static final int ic_action_received=0x7f020001;
+        public static final int ic_action_takepic=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int tile=0x7f020004;
     }
     public static final class id {
         public static final int container=0x7f080000;
-        public static final int gallery=0x7f080002;
-        public static final int picture=0x7f080004;
-        public static final int takepic=0x7f080003;
+        public static final int gallery=0x7f080003;
+        public static final int imageButton=0x7f080004;
+        public static final int picture=0x7f080005;
+        public static final int takepic=0x7f080002;
         public static final int texture=0x7f080001;
     }
     public static final class layout {
